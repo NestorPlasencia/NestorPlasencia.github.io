a521d02c5a1d7bab1533acc6bef21c8f552ed093
NestorPlasencia <nplasencia82@gmail.com>
2020-07-30 14:33:46 -0500

    correción de ortografía

--- postcss.config.js
+++ postcss.config.js
@@ -11,5 +11,22 @@ module.exports = {
         
         require('tailwindcss'),
         require('autoprefixer'),
+
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                '../**/*.html',
+                '../**/*.js',
+            ],
+            defaultExtractor: content => {
+                // Capture as liberally as possible, including things like `h-(screen-1.5)`
+                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+            
+                // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+            
+                return broadMatches.concat(innerMatches)
+            },
+        }),
+        require("postcss-css-variables")
     ],
 }
